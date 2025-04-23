2654103b38f8f218d31f5955cb01fc871c727c0d
HakimBenJelloul <hakimbenjelloul26@gmail.com>
2025-04-23 01:00:35 +0100

    ...

--- spring-boot-microservices-main/job-service/src/test/java/com/safalifter/jobservice/JobServiceApplicationTests.java
+++ spring-boot-microservices-main/job-service/src/test/java/com/safalifter/jobservice/JobServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.safalifter.jobservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Utiliser le profil test avec la configuration H2
 class JobServiceApplicationTests {
 
 	@Test
