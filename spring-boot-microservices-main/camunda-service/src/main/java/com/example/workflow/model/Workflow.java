1005c241dcc09b7c5035ee866e6266326a7daf9b
HakimBenJelloul <hakimbenjelloul26@gmail.com>
2025-05-17 01:22:56 +0100

    ...

--- spring-boot-microservices-main/camunda-service/src/main/java/com/example/workflow/model/Workflow.java
+++ spring-boot-microservices-main/camunda-service/src/main/java/com/example/workflow/model/Workflow.java
@@ -16,7 +16,7 @@ public class Workflow extends BaseEntity {
     @Column(name = "title", nullable = false, unique = true)
     private String title;
 
-    @Column(name = "workflow_content", nullable = false, columnDefinition = "TEXT")
+    @Column(name = "workflow_content", nullable = false, columnDefinition = "LONGTEXT")
     private String workflowContent;
 
     @Column(name = "ready_to_deploy", nullable = false, columnDefinition = "boolean default false")
